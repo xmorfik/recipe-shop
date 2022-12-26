e8ac59615a2d05b1edae094032e0419488f35520
Andrii <a.dukhno@outlook.com>
2022-12-26 15:18:56 +0200

    add recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+export class Recipe{
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name : string, desc: string, imgPath : string){
+        this.name = name;
+        this.description = desc;
+        this.imagePath = imgPath;
+    }
+}
