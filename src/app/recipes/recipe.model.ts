9bc23aff44072295460bf55f09d212158e9dc2a2
Andrii <a.dukhno@outlook.com>
2022-12-26 16:27:17 +0200

    add ingredients model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,6 @@
 export class Recipe{
-    public name: string;
-    public description: string;
-    public imagePath: string;
-
-    constructor(name : string, desc: string, imgPath : string){
-        this.name = name;
-        this.description = desc;
-        this.imagePath = imgPath;
-    }
+    constructor(
+        public name : string,
+        public description: string,
+        public imagePath : string){}
 }
