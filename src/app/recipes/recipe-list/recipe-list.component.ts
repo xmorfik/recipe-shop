0abf517640dc27912735f40825261b8a6cdf7c7c
Andrii <a.dukhno@outlook.com>
2022-12-26 15:54:48 +0200

    add cards

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Array<Recipe> = [
+    new Recipe('Burger','Burger','https://images.pexels.com/photos/3738730/pexels-photo-3738730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
     new Recipe('Burger','Burger','https://images.pexels.com/photos/3738730/pexels-photo-3738730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
   ];
 }
