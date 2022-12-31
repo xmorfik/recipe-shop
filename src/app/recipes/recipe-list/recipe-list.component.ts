995619f69030ed41a84c53d6a5535daa1e1542cb
Andrii Dukhno <a.dukhno@outlook.com>
2022-12-31 10:43:19 +0200

    add details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Array<Recipe> = [
     new Recipe('Burger','Burger','https://images.pexels.com/photos/3738730/pexels-photo-3738730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-    new Recipe('Burger','Burger','https://images.pexels.com/photos/3738730/pexels-photo-3738730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+    new Recipe('Pizza','Pizza','https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg')
   ];
 
   onRecipeSelected(recipe:Recipe){
